a2b81d37b90aea9139eb90e90ec7b6bbe14aeb7e
Angelo Poggi <angelo.poggi@opti9tech.com>
2022-03-08 07:35:09 -0500

    path fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import Pidash
+from pidash.pidash import PiDash
+#using absolute path here to import for testing
 
 if __name__ == "__main__":
-    pidashinit = PiDash
-    print(PiDash.pulsegenread())
+    pidashinit = PiDash()
+    print(pidashinit.pulsegenread())
