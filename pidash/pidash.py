d72c53d94760537bd3a0cd1d8b4662bc81e6a6e0
Angelo Poggi <angelo.poggi@opti9tech.com>
2022-03-08 08:48:57 -0500

    speed reading logic

--- pidash/pidash.py
+++ pidash/pidash.py
@@ -7,6 +7,9 @@ class PiDash:
     def pulsegenread(self):
         sen0105 = gpiozero.DigitalInputDevice(26,pull_up=True)
         while True:
-            speedReading = sen0105.value ++ sen0105.value
-            print(speedReading)
+            if sen0105.value == 1:
+                speedReading = 0
+                speedReading ++ 1
+                if speedReading >= 16000:
+                    print(speedReading)
 
