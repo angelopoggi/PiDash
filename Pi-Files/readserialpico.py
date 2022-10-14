5c2e451e6193c70bc03a561d683193f0b09afb43
Angelo Poggi <angelo.poggi@opti9tech.com>
2022-10-14 00:04:39 -0400

    uart testing

--- Pi-Files/readserialpico.py
+++ Pi-Files/readserialpico.py
@@ -10,17 +10,17 @@ class ReadPicoSerial():
         self.UART_BAUD = 9600
 
     def readpico(self):
-        ser = Serial(self.UART_COM,
-                     self.UART_BAUD,
-                     parity=PARITY_NONE,
-                     stopbits=STOPBITS_ONE,
-                     bytesize=EIGHTBITS,
-                     timeout=1
-                     )
-        time.sleep(1)
-        while True:
-            try:
-                data = str((ser.read(10)))
-                print(data)
-            except KeyboardInterrupt:
-                raise("KEYBOARD INTERUPT - OHMY!")
+        with open(self.UART_COM, 'rb') as ser
+            ser = Serial(self.UART_COM,
+                         self.UART_BAUD,
+                         parity=PARITY_NONE,
+                         stopbits=STOPBITS_ONE,
+                         bytesize=EIGHTBITS,
+                         timeout=1
+                         )
+            while True:
+                try:
+                    data = ser.read(2048)
+                    print(repr(data))
+                except KeyboardInterrupt:
+                    raise("KEYBOARD INTERUPT - OHMY!")
