9166c3c51ce095b60c2416dbed9c77288b383a21
Angelo Poggi <angelo.poggi@opti9tech.com>
2022-10-13 21:29:13 -0400

    uart testing

--- Pi-Files/readserialpico.py
+++ Pi-Files/readserialpico.py
@@ -6,12 +6,15 @@ import time
 
 class ReadPicoSerial():
     def __init__(self):
-        self.UART_COM = '/dev/tty'
+        self.UART_COM = 'COM14'
         self.UART_BAUD = 9600
     def readpico(self):
         ser = Serial(self.UART_COM, self.UART_BAUD)
         time.sleep(1)
         while True:
-            data = (ser.read(2))
-            received_data = str(data, 'UTF-8')
-            print(received_data)
+            try:
+                data = (ser.read(2))
+                received_data = str(data, 'UTF-8')
+                print(received_data)
+            except KeyboardInterrupt:
+                raise("KEYBOARD INTERUPT - OHMY!")
