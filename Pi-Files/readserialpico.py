b394efd286c5cdd8cf7f777757e7827193d6244e
Angelo Poggi <angelo.poggi@opti9tech.com>
2022-10-17 13:30:35 -0400

    serial testing

--- Pi-Files/readserialpico.py
+++ Pi-Files/readserialpico.py
@@ -21,7 +21,7 @@ class ReadPicoSerial():
             try:
                 data = ser.read(100)
                 print(data)
-                time.sleep(1)
+                utime.sleep(1)
                 # received_data = str(data, 'UTF-8')
                 # print(received_data)
             except KeyboardInterrupt:
